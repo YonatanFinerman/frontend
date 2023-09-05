40715ec32b959120b1814868553f8b282715df0a
yonatanfinerman <julianmode808@gmail.com>
2023-09-05 19:10:22 +0300

    added about

--- src/cmps/app-header.jsx
+++ src/cmps/app-header.jsx
@@ -1,9 +1,11 @@
-import { Link, NavLink } from 'react-router-dom'
+import { Link, NavLink, useLocation } from 'react-router-dom'
 
 export function AppHeader() {
 
+    const location = useLocation()
+
     return (
-        <header className="app-header flex align-center space-between">
+        <header className="app-header flex align-center space-between" style={{background:`${(location.pathname === '/')?'none':'red'}`}}>
             <img className='logo' src={`${require('../assets/img/logo.png')}`}  />
             <nav>
                 <p>About</p>
--- src/pages/home-page.jsx
+++ src/pages/home-page.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import { Link } from 'react-scroll'
 
 export function HomePage() {
 
@@ -10,9 +11,16 @@ export function HomePage() {
                 <div className='text-center'>
 
                 <h1>FineTune</h1>
-                <button > <p>Learn more!</p> </button>
+
+                <Link to='about' spy={true} smooth={true} offset={0} duration={500}><button > <p>Learn more!</p> </button></Link>
                 </div>
             </section>
+
+            <section className='about' id='about'>
+                <h2>About</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere deleniti quis odit incidunt ut minima porro accusamus odio blanditiis tenetur iure inventore perspiciatis architecto doloremque facilis, laudantium aliquam fugit. Deleniti!</p>
+            </section>
+
         </section >
     )
 }
